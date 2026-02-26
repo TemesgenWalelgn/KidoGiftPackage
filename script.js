9936cf6fc03d2cd1c9bb8088b27a1df20ffa3f98
TemesgenWalelgn <temulove79@gmail.com>
2026-02-26 14:32:39 +0300

    t

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function handleOrder(event) {
 // Initialize on Page Load
 // ==========================
 document.addEventListener('DOMContentLoaded', () => {
-    showTab('Valentine'); // set default tab
+    showTab('setoch'); // set default tab
     
        // Attach the click handler to all 'Order Now' buttons
     const orderButtons = document.querySelectorAll('.order-btn');
